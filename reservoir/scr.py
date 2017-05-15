0b40f84b7606d76319d081e4e8726aed45f3c751
1Reinier <reiniermaat@msn.com>
2017-05-15 18:21:29 -0400

    Added k-folds

--- reservoir/scr.py
+++ reservoir/scr.py
@@ -154,6 +154,7 @@ class SimpleCycleReservoir:
                 out_weights = np.linalg.solve(ridge_x, ridge_y).reshape(-1, 1)
             except np.linalg.LinAlgError:
                 # Pseudo-inverse solution
+                print('**FALLBACK**')
                 out_weights = scipy.linalg.pinvh(ridge_x, ridge_y, rcond=1e3*np.finfo('f').eps).reshape(-1, 1)  # Robust solution if ridge_x is singular
             
             # Validation set
